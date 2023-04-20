b12f47c321613983aaece3b985f3527209b8b963
Yuliya Shkretova <yuliya.shkretova@gmail.com>
2023-04-20 18:19:51 +0200

    Tests for form

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import webbrowser
+
+import pytest
+from selene.support.shared import browser
+
+
+@pytest.fixture(scope='session',autouse=True)
+def browser_setup():
+    browser.config.driver_name = 'chrome'
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 903
+    browser.config.window_height = 1166
+    # browser.driver.set_window_size(903, 1166)
+    browser.config.timeout = 10
+
+    yield
+    browser.quit()
