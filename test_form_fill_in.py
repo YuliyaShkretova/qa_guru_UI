deb800558e4bce31167583dbbf3def8776761582
YuliyaShkretova <129888062+YuliyaShkretova@users.noreply.github.com>
2023-05-18 14:28:57 +0200

    revise format of file

--- test_form_fill_in.py
+++ test_form_fill_in.py
@@ -11,7 +11,6 @@ def test_form_fill_in(browser_setup):
     browser.element('[id="userEmail"]').should(be.blank).type('y.shk@mail.ru')
     browser.element('[for="gender-radio-2"]').click()
     browser.element('[id="userNumber"]').should(be.blank).type('1234567890')
-    #   browser.element('[id="dateOfBirthInput"]').clear().type('13 Nov 1990').press_enter()
     browser.element('[id="dateOfBirthInput"]').click()
     browser.element('.react-datepicker__year-select').click()
     browser.element('option:nth-child(91)').click()
@@ -21,8 +20,7 @@ def test_form_fill_in(browser_setup):
     browser.element('[id="subjectsInput"]').should(be.blank).type('d')
     browser.element('[id=react-select-2-option-0]').double_click()
     browser.element('[for="hobbies-checkbox-3"]').click()
-    # browser.element('[id="uploadPicture"]').send_keys(r'C:\..\..\..\sticker.webp')
-    browser.element('[id="uploadPicture"]').send_keys(os.getcwd() + '/sticker.webp')
+    browser.element('[id="uploadPicture"]').send_keys(os.getcwd() + '/sticker.jpg')
     browser.element('[id="currentAddress"]').should(be.blank).type('Мурталь, Австрия')
     browser.element('[id="stateCity-label"]').perform(command.js.scroll_into_view)
     browser.element('[id="state"]').click()
@@ -35,4 +33,4 @@ def test_form_fill_in(browser_setup):
     browser.element('[id="example-modal-sizes-title-lg"]').should(have.text('Thanks for submitting the form'))
     browser.all('.table td:nth-child(2)').should(
         have.exact_texts('Юлия Шкретова', 'y.shk@mail.ru', 'Female', '1234567890', '13 November,1990', 'Hindi', 'Music',
-                         'sticker.webp', 'Мурталь, Австрия', 'Uttar Pradesh Agra'))
+                         'sticker.jpg', 'Мурталь, Австрия', 'Uttar Pradesh Agra'))
