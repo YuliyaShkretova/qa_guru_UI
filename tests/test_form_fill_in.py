55b379d08a39a922ab04b3883e9605a35ef847f1
YuliyaShkretova <129888062+YuliyaShkretova@users.noreply.github.com>
2023-05-30 17:34:48 +0200

    Allure

--- tests/test_form_fill_in.py
+++ tests/test_form_fill_in.py
@@ -3,8 +3,8 @@ from data.user import user
 
 
 def test_form_fill_in(setup_browser):
-    browser = setup_browser
-    registration = Registration(browser)
+
+    registration = Registration()
     registration.open()
     registration.fill_in(user)
     registration.submit()
