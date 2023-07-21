5e5e5fd687223a70af8dd1c741ce0ecab628e06f
YuliyaShkretova <129888062+YuliyaShkretova@users.noreply.github.com>
2023-07-21 11:32:21 +0200

    revised

--- utils/attach.py
+++ utils/attach.py
@@ -2,7 +2,6 @@ import allure
 from allure_commons.types import AttachmentType
 
 
-
 def add_screenshot(browser):
     png = browser.driver.get_screenshot_as_png()
     allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
@@ -13,9 +12,9 @@ def add_logs(browser):
     allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
 
 
-# def add_html(browser):
-#     html = browser.driver.page_source
-#     allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
+def add_html(browser):
+    html = browser.driver.page_source
+    allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
 
 
 def add_video(browser):
