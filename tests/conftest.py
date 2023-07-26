d017d71b062fa449604f98f9518a78f49ebb74e9
YuliyaShkretova <129888062+YuliyaShkretova@users.noreply.github.com>
2023-07-26 17:00:57 +0200

    add print for connection

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,7 +46,8 @@ def setup_browser(request):
     password = os.getenv('PASSWORD!')
 
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        # command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
     browser.config.driver = driver
